f7144611a19d519688585c82da88f6609b7ad4ba
Rafael Panegassi <rm.panegassi@gmail.com>
2025-05-16 13:52:45 -0300

    feat: script to understand camelot extract data from pdf

---
+++ src/pdf_viz.py
@@ -0,0 +1,26 @@
+import os
+import camelot
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+from loguru import logger
+
+file_name = "corretora_jornada_de_dados (1)"
+path = os.path.abspath(f"src/files/pdf/jornada/{file_name}.pdf")
+
+tables = camelot.read_pdf(
+    path,
+    flavor="stream",
+    table_areas=["72, 560, 492, 390"],
+    strip_text=".\n"
+)
+
+camelot.plot(tables[0], kind="contour")
+
+plt.show()
+
+logger.info("PDF visualization complete.")
+
+#72, 560
+#492, 390
+
